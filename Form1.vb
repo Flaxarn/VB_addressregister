6fe464c9278aeb40446abd9f3faadacebb3b50f7
Kim Ståhl <flaxarn85@gmail.com>
2021-11-16 12:44:39 +0200

    databaskoppling

--- Form1.vb
+++ Form1.vb
@@ -1,13 +1,37 @@
 ﻿' Globala variabler för projektet
 
-
-Private da As
-
 Public Class Form1
-    Private con As New OleDb.OleDbConnection()
-    Private ds As New DataSet
+    ' Kontakt för databaskopplingar
+    Private con As New OleDb.OleDbConnection          ' Connection
+    Private ds As New DataSet                           ' Tabeller
+    Private da As New OleDb.OleDbDataAdapter            ' Uppdatera tabeller
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Databas variabler
+        Dim dbProvider As String
+        Dim dbSource As String
+        Dim dbName As String
+        Dim dbPath As String
+        Dim sql As String
+
+        ' Kombinera sökväg och databasnamn till en connection string
+        dbProvider = "PROVIDER=Microsoft.ACE.OLEDB.12.0;"
+        dbName = "adresser.accdb"
+        dbPath = "C:\Users\Kim Ståhl\OneDrive\Dokument\GitHub\VB_addressregister"
+
+        dbSource = "Data Source= " & dbPath & "\" & dbName
+        con.ConnectionString = dbProvider & dbSource
+
+        ' Öppna databasen
+        con.Open()
+
+        ' Välj data att visa
+        sql = "SELECT * from Personer"
+
+        ' Koppla och fyll dataadaptern
+        da = New OleDb.OleDbDataAdapter(sql, con)
+        da.MissingSchemaAction = MissingSchemaAction.AddWithKey
+        da.Fill(ds, "Adressbok")
 
     End Sub
 End Class
